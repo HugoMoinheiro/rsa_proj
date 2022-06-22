9f0d0b3085cc0e160218c70270d56a9b21c7de01
pedromsilva99 <pedromsilva99@ua.pt>
2022-06-22 19:14:17 +0100

    javascript updating coordinates without refreshing, but not yet on google maps api

--- webproj/app/views.py
+++ webproj/app/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-from django.http import Http404, HttpResponse
 from datetime import datetime
 from pathlib import Path
+import time
+import json
+from django.http import JsonResponse
+
 
 filepath = Path(__file__).parent / "vehicle_coords.txt"
 
@@ -15,30 +18,33 @@ def home(request):
     return render(request, 'index.html', tparams)
 
 def get_coords(request):
+    start = time.time()
 
     car1_lat,car1_lon,car2_lat,car2_lon,car3_lat,car3_lon,car4_lat,car4_lon,car5_lat,car5_lon,car6_lat,car6_lon=[0]*12
     f = open(filepath, "r")
     for line in f:
         coords = line
         x = coords.split()
-        if x[0] == 1:
-            car1_lat = x[1]
-            car1_lon = x[2]
-        elif x[0] == 2:    
-            car2_lat = x[1]
-            car2_lon = x[2]
-        elif x[0] == 3: 
-            car3_lat = x[1]
-            car3_lon = x[2]
-        elif x[0] == 4: 
-            car4_lat = x[1]
-            car4_lon = x[2]
-        elif x[0] == 5:
-            car5_lat = x[1]
-            car5_lon = x[2]
-        elif x[0] == 6:
-            car6_lat = x[1]
-            car6_lon = x[2]
+        print(x)
+        if x[0] == '1':
+            print("ENTRA")
+            car1_lat = float(x[1])
+            car1_lon = float(x[2])
+        elif x[0] == '2':    
+            car2_lat = float(x[1])
+            car2_lon = float(x[2])
+        elif x[0] == '3': 
+            car3_lat = float(x[1])
+            car3_lon = float(x[2])
+        elif x[0] == '4': 
+            car4_lat = float(x[1])
+            car4_lon = float(x[2])
+        elif x[0] == '5':
+            car5_lat = float(x[1])
+            car5_lon = float(x[2])
+        elif x[0] == '6':
+            car6_lat = float(x[1])
+            car6_lon = float(x[2])
 
     tparams = {
         'title': 'Platoon Merging',
@@ -56,4 +62,9 @@ def get_coords(request):
         'car6_lat':car6_lat,
         'car6_lon':car6_lon,
     }
-    return render(request, 'index.html', tparams)
+    tparams = json.dumps(tparams)
+    loaded_r = json.loads(tparams)
+    end = time.time()
+    print(end - start)
+    
+    return JsonResponse(loaded_r)
